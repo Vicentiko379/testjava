e7a24bc78d7b27a33e69f901b99d24a76bdac70e
Vicente Noguez <v.noguez@ec-systems.de>
2023-11-27 12:35:40 -0300

    asdasd

---
+++ test/org/example/MainTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class MainTest{
+
+	@Test
+	public void shouldLoad() {
+		String [] arguments = {};
+		Main.main(arguments);
+	}
+
+}
